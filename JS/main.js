fe81ef7d7d85d3b0704508fec5e2b9cfc8410c53
Kinshuk Ghildial <dev.kghildial@gmail.com>
2017-12-20 13:39:46 +0530

    Set up XHR & JSON to JS conversion

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,19 @@
 var header = document.querySelector('header');
 var section = document.querySelector('section');
+
+//Storing JSON url in a variable
+var requestURL = 'https://raw.githubusercontent.com/Biuni/PokemonGOPokedex/master/pokedex.json';
+
+//XHR API used
+
+//Creating a new object instance of XHR
+var request = new XMLHttpRequest();
+
+// Using open() method for a 'GET' request
+request.open('GET', requestURL);
+
+//Setting repsonse type & conversion from JSON to JS object
+request.responseType = 'json';
+
+//Sending the request
+request.send();
