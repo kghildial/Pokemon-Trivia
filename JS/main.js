46527554be61245c3f2c50db485cbf55c375a49e
Kinshuk Ghildial <dev.kghildial@gmail.com>
2017-12-20 17:00:35 +0530

    Add comments to task1 function

--- JS/main.js
+++ JS/main.js
@@ -25,8 +25,10 @@ request.onload = function() {
   // task3(pokemons);
 }
 
+//Task one function
 function task1(jsonObj) {
   var pokemons = jsonObj['pokemon'], temp, swapped;
+  //Sorting pokemons in decreasing order of spawn_chance
   for(var i = 0; i < pokemons.length; i++) {
     swapped = false;
     for(var j = 0; j < pokemons.length-1; j++) {
@@ -41,34 +43,34 @@ function task1(jsonObj) {
       break;
     }
   }
-
+  //Creating table headings
   var tr_head = document.createElement('tr');
   p = pokemons[1];
   for(var key in p) {
     var th = document.createElement('th');
     th.textContent = key;
     tr_head.appendChild(th);
-    // console.log(key);
   }
   section.appendChild(tr_head);
 
-  for(i = 0; i < 10; i++) {
-    var tr = document.createElement('tr');
-    for(key in pokemons[i]) {
-      var td = document.createElement('td');
-      if(key === 'img') {
-        var img = document.createElement('img');
-        img.setAttribute('src', pokemons[i][key]);
-        td.appendChild(img);
+  //Filling tble with data
+  for(i = 0; i < 10; i++) { //Select first 10 entries of the sorted list
+    var tr = document.createElement('tr'); //Create 'tr' for each pokemon
+
+    for(key in pokemons[i]) { //Loop thorugh all the keys of each pokemon object
+      var td = document.createElement('td'); //Create a 'td' for each key
+      if(key === 'img') {  //If 'key' is img
+        var img = document.createElement('img'); //Create an 'img'
+        img.setAttribute('src', pokemons[i][key]); //Set 'img' URL as the 'key' value
+        td.appendChild(img); //Append 'img' to 'td'
       }
-      else if(key === 'next_evolution') {
-        for(j = 0; j < pokemons[i][key].length; j++) {
-          // console.log(pokemons[i][key][j]);
-          var evol = document.createElement('div');
-            evol.textContent = pokemons[i][key][j].name + ': ' + pokemons[i][key][j].num;
-            td.appendChild(evol);
+      else if(key === 'next_evolution') {  //If 'key' is 'next_evolution' array
+
+        for(j = 0; j < pokemons[i][key].length; j++) { //Loop through the array
+          var evol = document.createElement('div'); //Create a 'div' for each evolution
+            evol.textContent = pokemons[i][key][j].name + ': ' + pokemons[i][key][j].num; //Assign data to each 'div'
+            td.appendChild(evol); //Append the 'div' to the 'td'
         }
-        // console.log(pokemons[i][key].length);
       }
       else if(key === 'prev_evolution') {
         // console.log('prev_evol found for' + pokemons[i]['name']);
@@ -80,11 +82,10 @@ function task1(jsonObj) {
         }
       }
       else {
-        td.textContent = pokemons[i][key];
+        td.textContent = pokemons[i][key]; // Default case for adding data to the 'td'
       }
-      tr.appendChild(td);
-    }
-    section.appendChild(tr);
-    // console.log(pokemons[i]);
+      tr.appendChild(td); //Append all 'td' to the 'tr'
+    } // Data filled in all 'tr'
+    section.appendChild(tr); //Add the table to the section area of the page
   }
 }
