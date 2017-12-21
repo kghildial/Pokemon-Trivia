6fecdbfa87054933fcd9e7e5e5e68546c98af61a
Kinshuk Ghildial <dev.kghildial@gmail.com>
2017-12-21 12:28:00 +0530

    Revamp task2() code for all pokemons & repeating lists

--- JS/main.js
+++ JS/main.js
@@ -86,69 +86,76 @@ function task1(jsonObj) {
 function task2(jsonObj) {
   //Grouping Based on Evolutions
   //Pokemon name : Base Name (height, weight, spawn_time) --> Evolution 1 (height, weight, spawn_time) --> Evolution 2 (height, weight, spawn_time)
+  
   var pokemons = jsonObj['pokemon'];
 
   //Loop through all pokemons
-    //name = pokemons[i].name (add to 'p')
-    //if pokemon has no 'prev_evolution'
-      //add current (name, height, weight, spawn_time) to 'p'
-      //Loop through 'next_evolution'
-        //if pokemons[i].num == next_evolution[i].num
-          //add (name, height, weight, spawn_time) of that evolution to 'p'
-    //Else
-      //Loop through 'prev_evolution'
-        //if pokemons[i].num == prev_evolution[i].num
-          //add (name, height, weight, spawn_time) of that evolution ot 'p'
-      //add current (name, height, weight, spawn_time) to 'p'
-      //if pokemon has 'next_evolution'
-        //Loop through 'next_evolution'
-          //if pokemons[i].num == next_evolution.num
-          //add (name, height, weight, spawn_time) of that evolution to 'p'
-
-  var list = [];
-
   for(var i = 0; i < pokemons.length; i++) {
-    if(pokemons[i].prev_evolution) {
-      list.push(pokemons[i].prev_evolution[0].num);
-    }
-    else{
-      list.push(pokemons[i].num);
-    }
-  }
+    // create 'div' for each element
+    var div = document.createElement('div');
+    //create 'h3' for each element
+    var h3 = document.createElement('h3');
+    // create 'p' for each element
+    var p = document.createElement('p');
 
-  var uniqueList = Array.from(new Set(list));
+    //name = pokemons[i].name (add to 'p')
+    h3.innerHTML = pokemons[i].name + ': ';
 
-  var count = 0;
-  var div = document.createElement('div');
-  for(i = 0; i < pokemons.length; i++) {
-    for(var j = 0; j < uniqueList.length; j++) {
-      var inner_div = document.createElement('div')
-      inner_div.setAttribute('class', 'inner-div');
-      var h3 = document.createElement('h3');
-      var p = document.createElement('p');
-      if(pokemons[i].num == uniqueList[j]) {
-        var index = pokemons[i];
-        h3.innerHTML = index.name + ':';
-        p.innerHTML = index.name + ' (' + index.height + ', ' + index.weight + ', ' + index.spawn_time + ')';
-        if(index.next_evolution) {
-          var evol = [];
-          for(var k = 0; k < index.next_evolution.length; k++) {
-            for(var l = 0; l < pokemons.length; l++) {
-              // console.log(index.next_evolution[k].num);
-              if(pokemons[l].num == index.next_evolution[k].num) {
-                evol[k] = pokemons[l];
-                break;
-              }
+    //if pokemon has no 'prev_evolution'
+    if(!pokemons[i].prev_evolution) {
+      //add current (name, height, weight, spawn_time) to 'p'
+      p.innerHTML += pokemons[i].name + ' (' + pokemons[i].height + ', ' + pokemons[i].weight + ', ' + pokemons[i].spawn_time + ')';
+      //if pokemon has 'next_evolution'
+      if(pokemons[i].next_evolution) {
+        //Loop through 'next_evolution'
+        for(var j = 0; j < pokemons[i].next_evolution.length; j++) {
+          //if pokemons[i].num == next_evolution[i].num
+          for(var k = 0; k < pokemons.length; k++) {
+            if(pokemons[k].num == pokemons[i].next_evolution[j].num) {
+              //add (name, height, weight, spawn_time) of that evolution to 'p'
+              p.innerHTML += ' --> ' + pokemons[k].name + ' (' + pokemons[k].height + ', ' + pokemons[k].weight + ', ' + pokemons[k].spawn_time + ')';
+              break;
             }
-            console.log(evol[k]);
-            p.innerHTML += '-->' + evol[k].name + '(' + evol[k].height + ', ' + evol[k].weight + ', ' + evol[k].spawn_time + ')';
           }
         }
-        inner_div.appendChild(h3);
-        inner_div.appendChild(p);
-        div.appendChild(inner_div);
       }
-      section.appendChild(div);
     }
+    //Else (ie. if pokemons[i].prev_evolution exists)
+    else {
+      //Loop through 'prev_evolution'
+      for(j = 0; j < pokemons[i].prev_evolution.length; j++) {
+        //if pokemons[i].num == prev_evolution[i].num
+        for(k = 0; k < pokemons.length; k++) {
+          if(pokemons[k].num == pokemons[i].prev_evolution[j].num) {
+            //add (name, height, weight, spawn_time) of that evolution ot 'p'
+            p.innerHTML += pokemons[k].name + ' (' + pokemons[k].height + ', ' + pokemons[k].weight + ', ' + pokemons[k].spawn_time + ') --> ';
+          }
+        }
+      }
+      //add current (name, height, weight, spawn_time) to 'p'
+      if(pokemons[i].prev_evolution && pokemons[i].next_evolution) {
+        p.innerHTML += pokemons[i].name + ' (' + pokemons[i].height + ', ' + pokemons[i].weight + ', ' + pokemons[i].spawn_time + ') --> ';
+      }
+      else {
+        p.innerHTML += pokemons[i].name + ' (' + pokemons[i].height + ', ' + pokemons[i].weight + ', ' + pokemons[i].spawn_time + ')';
+      }
+      //if pokemon has 'next_evolution'
+      if(pokemons[i].next_evolution) {
+        //Loop through 'next_evolution'
+        for(var j = 0; j < pokemons[i].next_evolution.length; j++) {
+          //if pokemons[i].num == next_evolution[i].num
+          for(var k = 0; k < pokemons.length; k++) {
+            if(pokemons[k].num == pokemons[i].next_evolution[j].num) {
+              //add (name, height, weight, spawn_time) of that evolution to 'p'
+              p.innerHTML += pokemons[k].name + ' (' + pokemons[k].height + ', ' + pokemons[k].weight + ', ' + pokemons[k].spawn_time + ')';
+              break;
+            }
+          }
+        }
+      }
+    }
+    div.appendChild(h3);
+    div.appendChild(p);
+    section.appendChild(div);
   }
 }
